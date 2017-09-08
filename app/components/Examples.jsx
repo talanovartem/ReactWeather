0c231f5978611792ce7265810ccd848f30c97c5b
talanovartem <talanovartem@gmail.com>
2017-09-08 23:09:31 +0300

    add changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
+      <h3>Examples!</h3>
 		<p> Welcome!</p>
-      <h3>Examples Component!</h3>
       </div>
       )
 }
