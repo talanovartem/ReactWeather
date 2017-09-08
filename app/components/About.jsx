1b88b316db9be3780806bb19b06ec3d64c864cd2
talanovartem <talanovartem@gmail.com>
2017-09-09 00:18:25 +0300

    111

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,15 @@ var About = (props) => {
 	return (
 		<div>
 		<h3>About Component</h3>
-		<p>Some ne text to check how it works</p>
-		</div>)
+		<p>1</p>
+		<p>2</p>
+		<p>3</p>
+		<p>4</p>
+		<p>5</p>
+		<p>6</p>
+
+		</div>
+		)
 }
 
 module.exports = About;
